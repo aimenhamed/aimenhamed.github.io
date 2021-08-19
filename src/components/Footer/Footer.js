6dd22668c368ff89633f0fac8ef58334febb7927
Aimen <aimenhamed@live.com.au>
2021-08-19 12:41:31 +1000

    styling for nav

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 
-import { SocialIcons } from '../Header/HeaderStyles';
+import { SocialIcons } from '../Header/Header.style';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,12 +3,35 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './Header.style';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a style={{ display: "flex", alignItems: "center", color: "white" }}>
+          <DiCssdeck size="3rem" /> <span>Portfolio</span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+  </Container>
 );
 
 export default Header;
