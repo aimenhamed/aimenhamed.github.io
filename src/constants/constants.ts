ea9cba78e1d5beba1b3a1895b9167aa4ef3355be
Aimen <aimenhamed@live.com.au>
2021-08-19 13:37:09 +1000

    added projects

---
+++ src/constants/constants.ts
@@ -0,0 +1,48 @@
+export const projects = [
+  {
+    title: "WatchTime",
+    description:
+      "Watch Time is a centralised movie library to search any movie and see it's ratings and overview.",
+    image: "/images/watchtime.png",
+    tags: ["React", "TypeScript", "NodeJS", "Styled-Components"],
+    source: "https://github.com/aimenhamed/watchtime",
+    visit: "https://google.com",
+    id: 0,
+  },
+  {
+    title: "React Shop",
+    description: "E-Commerce shopping Page with intertactive cart using React",
+    image: "/images/reactshop.png",
+    tags: ["React", "TypeScript", "NodeJS", "Material-UI"],
+    source: "https://github.com/aimenhamed/react-shop",
+    visit: "https://google.com",
+    id: 1,
+  },
+  {
+    title: "Liqueur",
+    description:
+      "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+    image: "/images/liqueur.png",
+    tags: ["React", "JavaScript", "NodeJS", "Python", "Flask"],
+    source: "https://github.com/aimenhamed/seng2021-project",
+    visit: "https://google.com",
+    id: 2,
+  },
+  {
+    title: "Onboarding App",
+    description: "A Flutter project experimenting with images and UI design.",
+    image: "/images/onboard.png",
+    tags: ["Flutter", "Dart"],
+    source: "https://github.com/aimenhamed/onboard-page",
+    visit: "https://github.com/aimenhamed/onboard-page",
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "step1" },
+  { year: 2019, text: "step2" },
+  { year: 2020, text: "step3" },
+  { year: 2021, text: "step4" },
+];
