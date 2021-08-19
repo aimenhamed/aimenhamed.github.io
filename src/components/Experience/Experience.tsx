c88b11c6fa713d22e72909f39b0c201af733de2b
Aimen <aimenhamed@live.com.au>
2021-08-19 15:20:58 +1000

    experience section

---
+++ src/components/Experience/Experience.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Section, SectionDivider, SectionSubText, SectionTitle } from '../../styles/GlobalComponents/index.style';
+import { Box, Boxes, BoxNum, BoxText } from './Experience.style';
+
+const data = [
+  { company: "Optus", text: 'Product Engineer Intern', info: 'Full stack development using React and Node building UI for Optus Pause.' },
+  { company: "CommBank", text: 'Software Engineer Work Experience', info: 'Designing and maintaining systems for CommBank branches & development tools' },
+  { company: "Harvey Norman", text: 'CSR LiveChat', info: 'Customer service role with Harvey Norman online orders working in Agile environment' },
+];
+
+const Experience = () => (
+  <Section>
+    <SectionDivider />
+    <SectionTitle main>Experience</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <>
+        <Box key={index}>
+          <BoxNum>{card.company}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+        <SectionSubText>{card.info}</SectionSubText>
+        </>
+      ))}
+    </Boxes>
+  </Section>
+);
+
+export default Experience;
